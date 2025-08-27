e156a9f7a95cf298113c2bcf016ad9ef3d76924e
Yubaraj saha <yubarajsaha025@gmail.com>
2025-08-27 23:34:08 +0530

    Day02: added circle of area program

---
+++ area.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class area {
+    public static void main(String[] args) {
+        System.out.println("enter the redius of a circle");
+        Scanner sc =new Scanner(System.in);
+        
+        int redius=sc.nextInt();
+        double pai=3.14;
+
+        // area of the circle 
+        double area=pai*redius*redius;
+        System.out.println("the area of the circle is:\n "+area);
+    }
+    
+}
