33a5c16df0d3d6324d4d73292e2d498cdf669e45
Yubaraj saha <yubarajsaha025@gmail.com>
2025-08-27 23:19:25 +0530

    Day02:added sum of 2 numbers using user input

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String args[])
+    {
+        System.out.println("enter the numbers");
+        //use Scanner class for user input //
+        Scanner sc=new Scanner(System.in); 
+
+        int a = sc.nextInt();
+        int b =sc.nextInt();
+        int sum=a+b;
+        System.out.println("the sum of a+b\n"+sum);
+    }
+    
+}
