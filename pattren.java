ff17afc0b1563993c82f620d7f3d3d5b43e83872
YUBARAJ SAHA <140742080+Yubarajsaha@users.noreply.github.com>
2025-09-03 22:04:46 +0530

    updated

--- pattren.java
+++ pattren.java
@@ -6,7 +6,7 @@ public class pattren {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
 
-        for (int i = 1; i <=n; i++){//outer loop  
+        for (int i = 1; i <=n; i++){       //outer loop  
             
             for (int j= 1; j <=i; j++) {    //innner loop
                 System.out.print("*");
